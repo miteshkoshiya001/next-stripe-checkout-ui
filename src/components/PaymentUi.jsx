881ed27615187b37aa8956f4bdf81e4e92e97748
Nitin Bhalala <nitinbhalala5291@gmail.com>
2023-09-26 12:48:54 +0530

    Payment Ui

--- src/components/PaymentUi.jsx
+++ src/components/PaymentUi.jsx
@@ -4,19 +4,222 @@ import { AiFillApple, AiOutlineArrowLeft, AiOutlineDown, AiOutlineUp } from 'rea
 import { BsCreditCard, BsCreditCardFill } from 'react-icons/bs';
 
 const countries = [
-    'Country 1',
-    'Country 2',
-    'Country 3',
-    // Add more countries here
+    'Afghanistan',
+    'Albania',
+    'Algeria',
+    'Andorra',
+    'Angola',
+    'Antigua and Barbuda',
+    'Argentina',
+    'Armenia',
+    'Australia',
+    'Austria',
+    'Azerbaijan',
+    'Bahamas',
+    'Bahrain',
+    'Bangladesh',
+    'Barbados',
+    'Belarus',
+    'Belgium',
+    'Belize',
+    'Benin',
+    'Bhutan',
+    'Bolivia',
+    'Bosnia and Herzegovina',
+    'Botswana',
+    'Brazil',
+    'Brunei',
+    'Bulgaria',
+    'Burkina Faso',
+    'Burundi',
+    'Cabo Verde',
+    'Cambodia',
+    'Cameroon',
+    'Canada',
+    'Central African Republic',
+    'Chad',
+    'Chile',
+    'China',
+    'Colombia',
+    'Comoros',
+    'Congo (Congo-Brazzaville)',
+    'Costa Rica',
+    'Croatia',
+    'Cuba',
+    'Cyprus',
+    'Czechia (Czech Republic)',
+    'Democratic Republic of the Congo (Congo-Kinshasa)',
+    'Denmark',
+    'Djibouti',
+    'Dominica',
+    'Dominican Republic',
+    'East Timor (Timor-Leste)',
+    'Ecuador',
+    'Egypt',
+    'El Salvador',
+    'Equatorial Guinea',
+    'Eritrea',
+    'Estonia',
+    'Eswatini',
+    'Ethiopia',
+    'Fiji',
+    'Finland',
+    'France',
+    'Gabon',
+    'Gambia',
+    'Georgia',
+    'Germany',
+    'Ghana',
+    'Greece',
+    'Grenada',
+    'Guatemala',
+    'Guinea',
+    'Guinea-Bissau',
+    'Guyana',
+    'Haiti',
+    'Holy See',
+    'Honduras',
+    'Hungary',
+    'Iceland',
+    'India',
+    'Indonesia',
+    'Iran',
+    'Iraq',
+    'Ireland',
+    'Israel',
+    'Italy',
+    'Ivory Coast',
+    'Jamaica',
+    'Japan',
+    'Jordan',
+    'Kazakhstan',
+    'Kenya',
+    'Kiribati',
+    'Kosovo',
+    'Kuwait',
+    'Kyrgyzstan',
+    'Laos',
+    'Latvia',
+    'Lebanon',
+    'Lesotho',
+    'Liberia',
+    'Libya',
+    'Liechtenstein',
+    'Lithuania',
+    'Luxembourg',
+    'Madagascar',
+    'Malawi',
+    'Malaysia',
+    'Maldives',
+    'Mali',
+    'Malta',
+    'Marshall Islands',
+    'Mauritania',
+    'Mauritius',
+    'Mexico',
+    'Micronesia',
+    'Moldova',
+    'Monaco',
+    'Mongolia',
+    'Montenegro',
+    'Morocco',
+    'Mozambique',
+    'Myanmar (formerly Burma)',
+    'Namibia',
+    'Nauru',
+    'Nepal',
+    'Netherlands',
+    'New Zealand',
+    'Nicaragua',
+    'Niger',
+    'Nigeria',
+    'North Korea',
+    'North Macedonia (formerly Macedonia)',
+    'Norway',
+    'Oman',
+    'Pakistan',
+    'Palau',
+    'Palestine State',
+    'Panama',
+    'Papua New Guinea',
+    'Paraguay',
+    'Peru',
+    'Philippines',
+    'Poland',
+    'Portugal',
+    'Qatar',
+    'Romania',
+    'Russia',
+    'Rwanda',
+    'Saint Kitts and Nevis',
+    'Saint Lucia',
+    'Saint Vincent and the Grenadines',
+    'Samoa',
+    'San Marino',
+    'Sao Tome and Principe',
+    'Saudi Arabia',
+    'Senegal',
+    'Serbia',
+    'Seychelles',
+    'Sierra Leone',
+    'Singapore',
+    'Slovakia',
+    'Slovenia',
+    'Solomon Islands',
+    'Somalia',
+    'South Africa',
+    'South Korea',
+    'South Sudan',
+    'Spain',
+    'Sri Lanka',
+    'Sudan',
+    'Suriname',
+    'Sweden',
+    'Switzerland',
+    'Syria',
+    'Taiwan',
+    'Tajikistan',
+    'Tanzania',
+    'Thailand',
+    'Togo',
+    'Tonga',
+    'Trinidad and Tobago',
+    'Tunisia',
+    'Turkey',
+    'Turkmenistan',
+    'Tuvalu',
+    'Uganda',
+    'Ukraine',
+    'United Arab Emirates',
+    'United Kingdom',
+    'United States of America',
+    'Uruguay',
+    'Uzbekistan',
+    'Vanuatu',
+    'Venezuela',
+    'Vietnam',
+    'Yemen',
+    'Zambia',
+    'Zimbabwe'
 ];
+
 
 const PaymentUi = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [searchTerm, setSearchTerm] = useState('');
     const [isDetails, setIsDetails] = useState(false);
     const [isModal, setIsModal] = useState(false);
-
     const [scrollY, setScrollY] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubscribe = () => {
+        setIsLoading(true);
+
+        
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+    };
 
     const handleScroll = () => {
         setScrollY(window.scrollY);
@@ -40,14 +243,21 @@ const PaymentUi = () => {
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
     };
+
+    const selectCountry = (country) => {
+        // setSelectedCountry(country); // Update selectedCountry
+        setSearchTerm(country); // Update searchTerm
+        toggleDropdown();
+    }
+
     return (
         <>
             <div className='flex flex-col lg:flex-row items-center justify-center w-full min-h-screen'>
                 <div className='bg-black w-full lg:w-1/2 lg:min-h-screen flex flex-col items-center lg:items-end  gap-10 text-white relative 2xl:pt-16 '>
-                    <header className={`flex fixed  items-start justify-between w-full pt-3 px-5 lg:hidden ${isModal ? "bg-white" : "bg-black"} `}>
+                    <header className={`flex fixed  items-start justify-between w-full pt-3 px-5 lg:hidden ${(isModal && scrollY < 250) ? "bg-white" : "bg-black"} `}>
                         <div className='flex items-center gap-3  '>
                             <AiOutlineArrowLeft className='w-10 text-gray-400' />
-                            {isModal ? <Image src="/images/olevl.png"
+                            {(isModal && scrollY < 250) ? <Image src="/images/olevl.png"
                                 width="100"
                                 height="80"
                                 alt='olevl'
@@ -59,43 +269,45 @@ const PaymentUi = () => {
                                 />}
                         </div>
                         <button className='flex items-center justify-center gap-1 py-3 rounded-md text-gray-400 ' onClick={() => setIsModal(!isModal)}>
-                            <span className='underline decoration-dotted ' >{ scrollY > 250 ? "$125.00" : isModal ? "Close" : "Details"}</span>
+                            <span className='underline decoration-dotted ' >{scrollY > 250 ? "$125.00" : isModal ? "Close" : "Details"}</span>
                             {isModal ? <AiOutlineUp /> : <AiOutlineDown />
                             }
                         </button>
                     </header>
-                    {isModal && <div className={` absolute top-14 flex  justify-center gap-0 w-full flex-col bg-white px-7 `}>
-                        <div className='flex items-center justify-between gap-10 py-5 border-b border-gray-600'>
-                            <div className='flex flex-col items-start'>
-                                <span className='text-base font-semibold text-black '>Complete Shooter Club Payments</span>
-                                <span className='text-gray-400 text-sm font-medium ' >Billed monthly</span>
+                    {
+                        // (isModal && scrollY < 250) && 
+                        <div className={` ${(isModal && scrollY < 250) ? "slide-in" : "slide-out  -translate-y-[156%] "} absolute top-14 flex  justify-center gap-0 w-full flex-col bg-white px-7 `}>
+                            <div className='flex items-center justify-between gap-10 py-5 border-b border-gray-600'>
+                                <div className='flex flex-col items-start'>
+                                    <span className='text-base font-semibold text-black '>Complete Shooter Club Payments</span>
+                                    <span className='text-gray-400 text-sm font-medium ' >Billed monthly</span>
+                                </div>
+                                <div className='flex flex-col items-end justify-end'>
+                                    <span className='text-base font-semibold text-black '>$0.00</span>
+                                    <span className='text-gray-400 text-sm font-medium '>$125.00 / Month after</span>
+                                </div>
                             </div>
-                            <div className='flex flex-col items-end justify-end'>
-                                <span className='text-base font-semibold text-black '>$0.00</span>
-                                <span className='text-gray-400 text-sm font-medium '>$125.00 / Month after</span>
+                            <div className='flex items-start justify-between gap-10 py-5 border-b border-gray-600'>
+                                <div className='flex flex-col items-start gap-4' >
+                                    <span className='text-base font-semibold text-black '>Sub total</span>
+                                    <span className='text-gray-400 text-sm font-medium ' >Tax</span>
+                                </div>
+                                <div className='flex flex-col items-end gap-4'>
+                                    <span className='text-base font-semibold text-black '>$125.00</span>
+                                    <span className='text-gray-400 text-sm font-medium '>Enter address to calculate</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className='flex items-start justify-between gap-10 py-5 border-b border-gray-600'>
-                            <div className='flex flex-col items-start gap-4' >
-                                <span className='text-base font-semibold text-black '>Sub total</span>
-                                <span className='text-gray-400 text-sm font-medium ' >Tax</span>
+                            <div className='flex items-center justify-between gap-10 py-5 '>
+                                <div className='flex flex-col items-start gap-4'>
+                                    <span className='text-base text-black '>Total on October 15, 2023</span>
+                                    <span className='text-sm font-semibold text-black '>Total due today</span>
+                                </div>
+                                <div className='flex flex-col items-end gap-4'>
+                                    <span className='text-base text-black '>$125.00</span>
+                                    <span className='text-sm font-semibold text-black '>$0.00</span>
+                                </div>
                             </div>
-                            <div className='flex flex-col items-end gap-4'>
-                                <span className='text-base font-semibold text-black '>$125.00</span>
-                                <span className='text-gray-400 text-sm font-medium '>Enter address to calculate</span>
-                            </div>
-                        </div>
-                        <div className='flex items-center justify-between gap-10 py-5 '>
-                            <div className='flex flex-col items-start gap-4'>
-                                <span className='text-base text-black '>Total on October 15, 2023</span>
-                                <span className='text-sm font-semibold text-black '>Total due today</span>
-                            </div>
-                            <div className='flex flex-col items-end gap-4'>
-                                <span className='text-base text-black '>$125.00</span>
-                                <span className='text-sm font-semibold text-black '>$0.00</span>
-                            </div>
-                        </div>
-                    </div>}
+                        </div>}
                     <div className='flex flex-col items-center lg:items-start px-5 pb-10 lg:pb-0 lg:p-10 xl:p-16 xl:pt-16 gap-10 pt-16 '>
                         <div className='lg:flex items-center gap-3 hidden '>
                             <AiOutlineArrowLeft className='w-10 text-gray-400' />
@@ -117,7 +329,7 @@ const PaymentUi = () => {
                             <span className='text-lg text-gray-400 font-semibold text-center '>Then $125.00 per month starting on October 15, 2023</span>
                         </div>
 
-                        <div className={`lg:flex justify-center gap-2 flex-col ${isDetails ? "flex" : "hidden"} `}>
+                        <div className={`lg:flex justify-center gap-2 flex-col  hidden `}>
                             <div className='flex items-center justify-between gap-10 py-5 border-b border-gray-600'>
                                 <div className='flex flex-col items-start'>
                                     <span className='text-lg font-semibold  '>Complete Shooter Club Payments</span>
@@ -214,41 +426,18 @@ const PaymentUi = () => {
                                             value={searchTerm}
                                             onChange={(e) => setSearchTerm(e.target.value)}
                                         />
-                                        <svg
-                                            className={`w-4 h-4 transform ${isOpen ? ' transform  ' : ''
-                                                }`}
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                            onClick={toggleDropdown}
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d={isOpen ? 'M5 15l7-7 7 7' : 'M19 9l-7 7-7-7'}
-                                            />
-                                        </svg>
+                                        {
+                                            isOpen ? <AiOutlineUp className='w-4 h-4 cursor-pointer' onClick={() => toggleDropdown()} /> : <AiOutlineDown className='w-4 h-4 cursor-pointer' onClick={() => toggleDropdown()} />
+                                        }
                                     </div>
                                     {isOpen && (
-                                        <div className="absolute z-10 mt-2 w-full bg-white border border-gray-300 rounded-md shadow-lg">
-                                            {/* <input
-                                                type="text"
-                                                className="w-full px-4 py-2 border-b border-gray-300 focus:outline-none"
-                                                placeholder="Search..."
-                                                value={searchTerm}
-                                                onChange={(e) => setSearchTerm(e.target.value)}
-                                            /> */}
+                                        <div className="absolute z-10 mt-0 w-full bg-white border border-gray-300 rounded-b-md shadow-lg h-[200px] overflow-auto ">
                                             <ul>
                                                 {filteredCountries.map((country, index) => (
                                                     <li
                                                         key={index}
-                                                        className="px-4 py-2 hover:bg-indigo-100 cursor-pointer"
-                                                        onClick={() => {
-                                                            console.log(`Selected: ${country}`);
-                                                            toggleDropdown();
-                                                        }}
+                                                        className="px-4 py-1 hover:bg-indigo-100 cursor-pointer text-base font-medium"
+                                                        onClick={() => selectCountry(country)}
                                                     >
                                                         {country}
                                                     </li>
@@ -269,7 +458,19 @@ const PaymentUi = () => {
                                     </div>
                                 </div>
 
-                                <button type="submit" className='w-full bg-black py-3 rounded-lg mt-5 text-gray-500 text-xl font-semibold '>Subscribe</button>
+                                <button
+                                    // type="submit"
+                                    className={`w-full bg-black py-3 rounded-lg mt-5 text-gray-500 text-xl font-semibold relative ${isLoading ? 'opacity-100  h-[52px] cursor-not-allowed bg-black' : ''
+                                        }`}
+                                    onClick={handleSubscribe}
+                                    disabled={isLoading}
+                                >
+                                    {/* {isLoading && (
+                                        <div className="loader-spinner"></div>
+                                    )} */}
+                                    {isLoading ? <div className="loader-spinner "></div> : 'Subscribe'}
+                                </button>
+
                                 <div className='text-center'>
                                     <p className='text-center text-base font-semibold text-gray-400 '>By confirming your subscription, you allow levl to charge your card for this payment and future payment in acordance with their terms. you can always cancel your subscription.</p>
                                 </div>
